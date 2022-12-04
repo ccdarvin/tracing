9f2b4958c499cf93d6e73ddc631c53ff7b1f54f8
Darvin <cc.darvin@gmail.com>
2022-12-04 15:11:35 -0500

    logging

--- scripts/core/models.py
+++ scripts/core/models.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from  typing import Optional
 import redis
 
-r = redis.Redis(
+redis_conn = redis.Redis(
     host='redis-12622.c277.us-east-1-3.ec2.cloud.redislabs.com',
     port=12622,
     password='TxVYpEfg4DwZSjDxerOiWxNEhgIZouKa'
@@ -21,6 +21,6 @@ class Game(JsonModel):
     lastUpdate: datetime = Field(default=datetime.now(timezone.utc))
     
     class Meta:
-        database = r
+        database = redis_conn
         global_key_prefix = 'tracker'
         model_key_prefix = 'games'
--- scripts/jobs.py
+++ scripts/jobs.py
@@ -1,22 +1,49 @@
-import threading
-import time
-import schedule
+from sentry_sdk.integrations.logging import LoggingIntegration
 from scrapings.betano import scraping_games as betano
 from scrapings.betway import scraping_games as betway
+import sentry_sdk
+import threading
+import schedule
+import logging
+import time
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="jobs_log.log",
+    filemode="w",
+    format='%(name)s %(asctime)s %(levelname)s %(message)s'
+)
+
+sentry_logging = LoggingIntegration(
+    #level=logging.INFO,        # Capture info and above as breadcrumbs
+    event_level=logging.ERROR  # Send errors as events
+)
+
+sentry_sdk.init(
+    dsn="https://f7b61367ec5f472eb4b989913b5879b1@o220382.ingest.sentry.io/4504272342155264",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    integrations=[
+        sentry_logging,
+    ],
+)
 
 
 def job():
-    print('I am working... %s' % time.ctime())
+    logging.info("I am working...")
+    print('I am working...')
+    
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
     
 
-schedule.every(1).to(3).hours.do(run_threaded, betano)
-schedule.every(1).to(3).hours.do(run_threaded, betway)
-schedule.every(1).minutes.do(run_threaded, job)
-
+schedule.every(15).to(30).minutes.do(run_threaded, betano)
+schedule.every(15).to(30).minutes.do(run_threaded, betway)
+schedule.every(60).seconds.do(run_threaded, job)
 
 while 1:
     schedule.run_pending()
