7f96473287294008288c562bae5605b12f9cbac6
Darvin <cc.darvin@gmail.com>
2022-12-15 13:51:54 -0500

    add

--- scripts/core/webdriver.py
+++ scripts/core/webdriver.py
@@ -16,7 +16,8 @@ def init_driver(headless=True):
     options.add_experimental_option('useAutomationExtension', False)
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     stealth(driver,
-        languages=["es", "es"],
+        user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
+        languages=["es-PE", "es"],
         vendor="Google Inc.",
         platform="Win32",
         webgl_vendor="Intel Inc.",
